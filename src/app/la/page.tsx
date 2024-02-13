f92cab3226c1b39b0dc4122ff225c083e0e61921
Allison Casey <allie.jo.casey@gmail.com>
2024-02-12 20:31:39 -0800

    refactor: cleanup safe routes map component interface

--- src/app/la/page.tsx
+++ src/app/la/page.tsx
@@ -7,6 +7,14 @@ import DescriptionControlPanel from "@/app/components/DescriptionControlPanel";
 const fetcher: Fetcher<GeoJSON.GeoJSON, string> = (url) =>
   fetch(url).then((r) => r.json());
 
+const BOUNDS: MapboxGeocoder.Bbox = [
+  -118.88065856936811,
+  33.63722119725411, // Southwest coordinates
+  -117.83375850298786,
+  34.4356118682199, // Northeast coordinates
+];
+const CENTER = [-118.35874251099995, 34.061734936928694];
+
 export default function SafeRoutesLA() {
   const { data } = useSWR("/api/la", fetcher);
 
@@ -14,6 +22,13 @@ export default function SafeRoutesLA() {
     <SafeRoutesMap
       routes={data}
       controlPanelContent={<DescriptionControlPanel />}
+      initialViewState={{
+        longitude: CENTER[0],
+        latitude: CENTER[1],
+        zoom: 12,
+      }}
+      maxBounds={BOUNDS}
+      geocoderBbox={BOUNDS}
     />
   );
 }
