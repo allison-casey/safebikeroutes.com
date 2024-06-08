42427f14dd7ac212b62ecd1cea9612dc70047f6f
Allison Casey <allie.jo.casey@gmail.com>
2024-06-08 13:40:29 -0700

    feat: authorize user after authentication

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import github from "next-auth/providers/github";
+import { NextResponse } from "next/server";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [github],
@@ -7,8 +8,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnAdminPanel = nextUrl.pathname.endsWith('/admin');
+      const isAuthorized = isLoggedIn && auth.user?.email === 'allie.jo.casey@gmail.com'
       if (isOnAdminPanel) {
-        return isLoggedIn
+        if (isAuthorized) {
+          return true
+        } else {
+          return NextResponse.redirect(new URL('/la', nextUrl.origin))
+        }
       }
       return true;
     },
