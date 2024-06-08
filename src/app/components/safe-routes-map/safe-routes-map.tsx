8daa390ff47ff9650402444f2909399bc6857c91
Allison Casey <allie.jo.casey@gmail.com>
2024-06-08 13:19:07 -0700

    fix: merge main

--- src/app/components/safe-routes-map/safe-routes-map.tsx
+++ src/app/components/safe-routes-map/safe-routes-map.tsx
@@ -1,21 +1,35 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { clsx } from "clsx";
-import mapboxgl from "mapbox-gl";
-import { ReactElement, useRef, useState } from "react";
-import Map, {
+import mapboxgl, { GeolocateControl as IGeolocateControl } from "mapbox-gl";
+import { ReactElement, useCallback, useState } from "react";
+import {
   GeolocateControl,
+  Map,
   Layer,
   MapProps,
   MapRef,
   Source,
+  useMap,
+  MapProvider,
 } from "react-map-gl";
 import { legacyRouteStyles, routeStyles } from "../../route_styles";
 import GeocoderControl from "./geocoder-control";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
-const DEFAULT_MAP_STYLE = "Streets";
-const MAP_STYLES = [
+type Styles = "Streets" | "Satellite Streets";
+type WatchState =
+  | "OFF"
+  | "ACTIVE_LOCK"
+  | "WAITING_ACTIVE"
+  | "ACTIVE_ERROR"
+  | "BACKGROUND"
+  | "BACKGROUND_ERROR";
+
+const DEFAULT_MAP_STYLE: Styles = "Streets";
+const MAP_STYLES: { title: Styles; style: string }[] = [
   {
     title: "Streets",
     style: "mapbox://styles/mapbox/streets-v12",
@@ -30,43 +44,46 @@ const ControlPanelButton = ({
   onClick,
 }: {
   showControlPanel: boolean;
-  onClick: any;
-}) => (
-  <div
-    className="absolute flex right-[calc(50%-1rem)] bottom-0 md:right-2 mb-2 md:mb-10 md:bottom-0 z-20 px-4 py-2 rounded-lg bg-white drop-shadow-md"
-    onClick={onClick}
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className={clsx([
-        "w-4",
-        "h-4",
-        showControlPanel ? "rotate-90" : "-rotate-90",
-        showControlPanel ? "md:rotate-0" : "md:rotate-180",
-      ])}
+  onClick: (map: MapRef | undefined) => void;
+}) => {
+  const { la } = useMap();
+  return (
+    <div
+      className="absolute flex right-[calc(50%-1rem)] bottom-0 md:right-2 mb-2 md:mb-10 md:bottom-0 z-20 px-4 py-2 rounded-lg bg-white drop-shadow-md"
+      onClick={() => onClick(la)}
     >
-      <path
-        fillRule="evenodd"
-        d="M13.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
-        clipRule="evenodd"
-      />
-      <path
-        fillRule="evenodd"
-        d="M19.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
-        clipRule="evenodd"
-      />
-    </svg>
-  </div>
-);
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className={clsx([
+          "w-4",
+          "h-4",
+          showControlPanel ? "rotate-90" : "-rotate-90",
+          showControlPanel ? "md:rotate-0" : "md:rotate-180",
+        ])}
+      >
+        <path
+          fillRule="evenodd"
+          d="M13.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
+          clipRule="evenodd"
+        />
+        <path
+          fillRule="evenodd"
+          d="M19.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+  );
+};
 
 const StyleSelector = ({
   currentlySelectedStyle,
   onClick,
 }: {
-  currentlySelectedStyle: string;
-  onClick: (title: string) => any;
+  currentlySelectedStyle: Styles;
+  onClick: (title: Styles) => void;
 }) => (
   <div className="absolute flex left-2 bottom-0 mb-12 z-20 rounded-lg drop-shadow-md">
     {MAP_STYLES.map(({ title }) => (
@@ -115,9 +132,25 @@ const SafeRoutesMap = ({
   }
 
   const styles = useLegacyStyles ? legacyRouteStyles : routeStyles;
-  const mapRef = useRef<MapRef>(null);
-  const [currentStyle, setCurrentStyle] = useState(DEFAULT_MAP_STYLE);
-  const [showControlPanel, toggleControlPanel] = useState(true);
+  const [currentStyle, setCurrentStyle] = useLocalStorage<Styles>(
+    "map-style",
+    DEFAULT_MAP_STYLE,
+  );
+  const [showControlPanel, toggleControlPanel] = useLocalStorage(
+    "display-panel",
+    true,
+  );
+  const [geolocationEnabled, setGeolocationEnabled] = useLocalStorage(
+    "geolocation-enabled",
+    false,
+  );
+
+  const [geolocater, setGelocater] = useState<IGeolocateControl | null>(null);
+  const geolocateRef = useCallback(
+    (node: IGeolocateControl) => setGelocater(node),
+    [],
+  );
+  console.log(routes)
 
   const layers = styles
     .map(({ routeType, paintLayers }) =>
@@ -127,68 +160,91 @@ const SafeRoutesMap = ({
           id={`saferoutesla-${routeType}-${index}`}
           type="line"
           source="saferoutesla"
-          filter={["==", "routeType", routeType]}
+          filter={["==", "route_type", routeType]}
           paint={paintLayer}
+          beforeId="road-label"
         />
       )),
     )
     .flat();
 
   return (
-    <div className="w-dvh h-dvh grid grid-rows-[1fr_auto] grid-cols-1 md:grid-cols-[1fr_auto] md:grid-rows-1">
-      <div className="relative">
-        <Map
-          mapboxAccessToken={token}
-          mapLib={mapboxgl}
-          mapStyle={MAP_STYLES.find((d) => d.title === currentStyle)?.style}
-          ref={mapRef}
-          {...mapboxProps}
-        >
-          {routes ? (
-            <Source id="saferoutesla" type="geojson" data={routes}>
-              {...layers}
-            </Source>
-          ) : null}
-          <GeocoderControl
+    <MapProvider>
+      <div className="w-dvh h-dvh grid grid-rows-[1fr_auto] grid-cols-1 md:grid-cols-[1fr_auto] md:grid-rows-1">
+        <div className="relative">
+          <Map
+            id="la"
             mapboxAccessToken={token}
-            position="top-right"
-            bbox={geocoderBbox}
+            mapLib={mapboxgl}
+            mapStyle={MAP_STYLES.find((d) => d.title === currentStyle)?.style}
+            onLoad={() => {
+              if (geolocater) {
+                if (geolocationEnabled) {
+                  geolocater.trigger();
+                }
+                geolocater.on("trackuserlocationstart", () => {
+                  setGeolocationEnabled(true);
+                });
+                geolocater.on("trackuserlocationend", () => {
+                  // NOTE: accessing internal property so have to do this
+                  // janky typescript shenanigans
+                  const watchState: WatchState = (geolocater as any)
+                    ._watchState;
+                  if (watchState === "OFF") {
+                    setGeolocationEnabled(false);
+                  }
+                });
+              }
+            }}
+            {...mapboxProps}
+          >
+            {routes ? (
+              <Source id="saferoutesla" type="geojson" data={routes}>
+                {...layers}
+              </Source>
+            ) : null}
+            <GeocoderControl
+              mapboxAccessToken={token}
+              position="top-right"
+              bbox={geocoderBbox}
+            />
+            <GeolocateControl
+              trackUserLocation
+              showUserHeading
+              positionOptions={{ enableHighAccuracy: true }}
+              position="top-left"
+              ref={geolocateRef}
+            />
+          </Map>
+          <StyleSelector
+            onClick={(title) => setCurrentStyle(title)}
+            currentlySelectedStyle={currentStyle}
           />
-          <GeolocateControl
-            trackUserLocation
-            showUserHeading
-            positionOptions={{ enableHighAccuracy: true }}
-            position="top-left"
+          <ControlPanelButton
+            showControlPanel={showControlPanel}
+            onClick={(map) => {
+              toggleControlPanel(!showControlPanel);
+              setTimeout(() => map && map.resize(), 150);
+            }}
           />
-        </Map>
-        <StyleSelector
-          onClick={(title) => setCurrentStyle(title)}
-          currentlySelectedStyle={currentStyle}
-        />
-        <ControlPanelButton
-          showControlPanel={showControlPanel}
-          onClick={() => {
-            toggleControlPanel(!showControlPanel);
-            setTimeout(() => mapRef.current?.resize(), 150);
-          }}
-        />
+        </div>
+        <div
+          className={clsx([
+            showControlPanel ? "h-[300px] p-5" : "h-0 p-0",
+            showControlPanel ? "md:w-[400px] p-5" : "w-0 p-0",
+            "md:h-auto",
+            "transition",
+            "transition-all",
+            "overflow-y-auto",
+            "bg-white",
+            "drop-shadow-md",
+          ])}
+        >
+          {controlPanelContent}
+        </div>
       </div>
-      <div
-        className={clsx([
-          showControlPanel ? "h-[300px] p-5" : "h-0 p-0",
-          showControlPanel ? "md:w-[400px] p-5" : "w-0 p-0",
-          "md:h-auto",
-          "transition",
-          "transition-all",
-          "overflow-y-auto",
-          "bg-white",
-          "drop-shadow-md",
-        ])}
-      >
-        {controlPanelContent}
-      </div>
-    </div>
+    </MapProvider>
   );
 };
 
-export default SafeRoutesMap;
+export default dynamic(() => Promise.resolve(SafeRoutesMap), { ssr: false });
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { SessionProvider } from "next-auth/react";
+import { MapProvider } from "react-map-gl";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
