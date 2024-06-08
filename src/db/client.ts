fe096ab083ee3eae0ff28080df51694df4422e4d
Allison Casey <allie.jo.casey@gmail.com>
2024-06-08 12:53:22 -0700

    chore: move to vercel postgres

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,22 +1,8 @@
-import { Pool } from "pg";
-import { Kysely, PostgresDialect } from "kysely";
 import { DB } from "kysely-codegen";
-
-const dialect = new PostgresDialect({
-  pool: new Pool({
-    database: "db",
-    host: "localhost",
-    user: "postgres",
-    password: "postgres",
-    port: 5432,
-    max: 10,
-  }),
-});
+import { createKysely } from '@vercel/postgres-kysely';
 
 // Database interface is passed to Kysely's constructor, and from now on, Kysely
 // knows your database structure.
 // Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
 // to communicate with your database.
-export const db = new Kysely<DB>({
-  dialect,
-});
+export const db = createKysely<DB>();
