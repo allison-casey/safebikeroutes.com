751b03b1cd25a20577f4707a2f320aed502045d2
Allison Casey <allisoncasey@Allisons-Laptop.local>
2024-06-09 14:53:07 -0700

    chore: try not calling db

--- auth.ts
+++ auth.ts
@@ -1,20 +1,21 @@
 import NextAuth from "next-auth";
 import github from "next-auth/providers/github";
 import PostgresAdapter from "@auth/pg-adapter";
-import { db, pool } from "@/db/client";
+import { pool } from "@/db/client";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [github],
   adapter: PostgresAdapter(pool),
   callbacks: {
     async session({ session, user }) {
-      const userRoles = await db
-        .selectFrom("user_roles")
-        .selectAll()
-        .where("userId", "=", user.id)
-        .execute();
-
-      session.user.roles = userRoles;
+      // const userRoles = await db
+      //   .selectFrom("user_roles")
+      //   .selectAll()
+      //   .where("userId", "=", user.id)
+      //   .execute();
+      //
+      // session.user.roles = userRoles;
+      session.user.roles = [];
       return session;
     },
     async authorized({ auth, request: { nextUrl } }) {
