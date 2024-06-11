39f7deb17fae615214bd42936ecd578d89d6dfc3
Allison Casey <allisoncasey@Allisons-Laptop.local>
2024-06-10 17:27:02 -0700

    chore: delete local file

--- playground.sql
+++
@@ -1,6 +0,0 @@
-     SELECT json_build_object(
-     'type', 'FeatureCollection',
-     'features', coalesce(json_agg(ST_AsGeoJSON(r.*)::json), '[]'::json)
-     ) AS feature_collection
-     FROM route r
-     WHERE region = 'LA'::"Region"
