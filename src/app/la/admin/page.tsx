de763700d3ae7df28411bc20c1f2626a0876057b
Allison Casey <acasey@twochairs.com>
2025-06-23 10:08:08 -0700

    chore: undo dev comments

--- src/app/la/admin/page.tsx
+++ src/app/la/admin/page.tsx
@@ -28,22 +28,18 @@ const permittedRoles = new Set<Role>(["ADMIN", "CONTRIBUTOR"]);
 
 export default async function SafeRoutesLA() {
   noStore();
-  // const routes = await getRoutes("LA");
-  // const session = await auth();
-  // if (!session?.user.roles.some((role) => permittedRoles.has(role.role))) {
-  //   return (
-  //     <main className="flex items-center justify-center md:h-screen">
-  //       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-  //         <Typography variant="h3">Not Authorized</Typography>
-  //       </div>
-  //     </main>
-  //   );
-  // }
+  const routes = await getRoutes("LA");
+  const session = await auth();
+  if (!session?.user.roles.some((role) => permittedRoles.has(role.role))) {
+    return (
+      <main className="flex items-center justify-center md:h-screen">
+        <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+          <Typography variant="h3">Not Authorized</Typography>
+        </div>
+      </main>
+    );
+  }
 
-  const routes: IRouteFeatureCollection = {
-    type: "FeatureCollection",
-    features: [],
-  };
   return (
     <SafeRoutesMapAdmin
       token={process.env.ACCESS_TOKEN}
