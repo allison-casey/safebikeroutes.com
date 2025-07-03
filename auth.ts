31b534b58a9560816128649e486f332ca8ba12e0
Allison Casey <acasey@twochairs.com>
2025-07-03 12:10:17 -0700

    feat: add google auth provider

--- auth.ts
+++ auth.ts
@@ -2,9 +2,10 @@ import { db, pool } from "@/db/client";
 import PostgresAdapter from "@auth/pg-adapter";
 import NextAuth from "next-auth";
 import github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [github],
+  providers: [github, Google],
   adapter: PostgresAdapter(pool),
   callbacks: {
     async session({ session, user }) {
